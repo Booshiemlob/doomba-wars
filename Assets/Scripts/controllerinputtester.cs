a866bf72c9a71088422305e0b0fc7bca83dda8b7
Kent Vuong <kentvuong88@gmail.com>
2020-03-03 16:25:09 +0800

    finished movement + multiplayer

--- Assets/Scripts/controllerinputtester.cs
+++ Assets/Scripts/controllerinputtester.cs
@@ -50,10 +50,34 @@ public class controllerinputtester : MonoBehaviour
 
     void Start()
     {
-        DebugText.text = "yay";
+       
     }
     void Update()
     {
+        //Get Joystick Names
+        string[] temp = Input.GetJoystickNames();
+
+        //Check whether array contains anything
+        if (temp.Length > 0)
+        {
+            //Iterate over every element
+            for (int i = 0; i < temp.Length; ++i)
+            {
+                //Check if the string is empty or not
+                if (!string.IsNullOrEmpty(temp[i]))
+                {
+                    //Not empty, controller temp[i] is connected
+                    Debug.Log("Controller " + i + " is connected using: " + temp[i]);
+                }
+                else
+                {
+                    //If it is empty, controller i is disconnected
+                    //where i indicates the controller number
+                    Debug.Log("Controller: " + i + " is disconnected.");
+
+                }
+            }
+        }
         // Camera Rig Movement Control
         hAxis = Input.GetAxis("Horizontal");
         vAxis = Input.GetAxis("Vertical");
