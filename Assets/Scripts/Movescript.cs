5ea8bfe267647a651c45cfd59122c0ed50ae60ba
VTRYG <51437086+VTRYG@users.noreply.github.com>
2020-03-10 13:13:14 +0800

    Rotating and Sucking
    - Rotating basics work.
    - Sucking debug works.

--- Assets/Scripts/Movescript.cs
+++ Assets/Scripts/Movescript.cs
@@ -9,6 +9,7 @@ public class Movescript : MonoBehaviour
     public float speed = 6.0f;
     public float jumpSpeed = 8.0f;
     public float gravity = 20.0f;
+    public float rotateSpeed = 3.0f;
     private Rigidbody rb;
     private Transform transformMove;
 
@@ -24,6 +25,9 @@ public class Movescript : MonoBehaviour
         moveDirection = new Vector3(Input.GetAxis("Horizontal"), 0.0f, Input.GetAxis("Vertical"));
         moveDirection *= speed;
         this.transform.position += moveDirection;
-        Debug.Log(Input.GetAxis("Horizontal"));
+       // Debug.Log(Input.GetAxis("Horizontal"));
+
+        //Rotate Player
+        transform.Rotate(0, Input.GetAxis("Horizontal"), 0);
     }
 }
