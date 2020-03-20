3dbe73ae799791124b289e09bc971adf6299546b
Spikesalive <48239825+Spikesalive@users.noreply.github.com>
2020-03-20 12:46:25 +0800

    time script

---
+++ Assets/TimerScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+    public float time;
+    // Start is called before the first frame update
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if(time <= 0)
+        {
+
+        }
+    }
+}
