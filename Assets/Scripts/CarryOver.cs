f95f3f1420bd595c32fa73640d588e3484e3bc33
daedalus13 <daedalus3909@gmail.com>
2020-03-20 16:46:37 +0800

    trouble

---
+++ Assets/Scripts/CarryOver.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarryOver : MonoBehaviour
+{
+public float score1;
+public float score2;
+public float score3;
+public float score4;
+
+   
+    void Awake()
+    {
+        
+        DontDestroyOnLoad (transform.gameObject);
+    }
+}
---
+++ Assets/Scripts/ScoreUI.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ScoreUI : MonoBehaviour
+{
+        public Text scoreBoard1;
+        public Text scoreBoard2;
+        public Text scoreBoard3;
+        public Text scoreBoard4;
+        public CarryOver source;
+
+    void start()
+    {
+        source = FindObjectOfType<CarryOver>();
+    }
+
+    void Update()
+    {
+        scoreboard1.text = source.score1.tostring();
+        scoreboard2.text = source.score2.tostring();
+        scoreboard3.text = source.score3.tostring();
+        scoreboard4.text = source.score4.tostring();
+
+    } 
+    
+    
+}
